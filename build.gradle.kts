2ea0a6e976af71988a7a00733836b9399a4e2af2
kioba <charlssom@gmail.com>
2024-08-25 19:25:16 +0200

    :bookmark: release 0.0.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,6 @@ plugins {
   alias(libs.plugins.compose.compiler).apply(false)
   alias(libs.plugins.androidLibrary).apply(false)
   alias(libs.plugins.kotlinMultiplatform).apply(false)
-  alias(libs.plugins.kmpNativeCoroutines) apply false
+  alias(libs.plugins.kmpNativeCoroutines).apply(false)
+  alias(libs.plugins.vaniktechMavenPublish).apply(false)
 }
-
-// tasks.register('clean', Delete) {
-//  delete rootProject.buildDir
-// }
